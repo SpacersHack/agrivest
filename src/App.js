a1da3c3a9c15090326dc3bb68d4d7ffb08bc96a1
haybuka <paschal.nduleo@gmail.com>
2023-10-15 01:09:41 +0100

    starting up agrivest

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import logo from './logo.svg';
-import './App.css';
+// import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App bg-red-400">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p className='text-blue-700'>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
