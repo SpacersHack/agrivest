e57a35b35d0cd6afd16593800ac3ac4ae6c94936
haybuka <paschal.nduleo@gmail.com>
2023-10-15 13:51:20 +0100

    signup page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import { Route, Routes } from 'react-router-dom';
 import Sidebar from './layout/sidebar';
 import Dashboard from './pages/dashboard';
-import Login from './pages/auth/login/login';
+import Login from './pages/auth/login';
 import './App.css'
+import Register from './pages/auth/register';
 
 function App() {
   return (
     <Routes>
       <Route path='/login' element={<Login />} />
-      <Route path='/register' element={<p>register</p>} />
+      <Route path='/register' element={<Register />} />
       <Route path='/' element={<Sidebar />}>
         <Route index element={<Dashboard />} />
         <Route path='support' element={<p className='h-full w-full'>support</p>} />
